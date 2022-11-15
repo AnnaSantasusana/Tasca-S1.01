22597c87c0911a22bc016600a4b61ab118a9e3d8
AnnaSantasusana <annasberch@gmail.com>
2022-11-15 19:24:06 +0100

    n1exercici2_AnnaSantasusana

--- n1exercici2_AnnaSantasusana/src/n1exercici2_AnnaSantasusana/Main_Car.java
+++ n1exercici2_AnnaSantasusana/src/n1exercici2_AnnaSantasusana/Main_Car.java
@@ -4,13 +4,15 @@ public class Main_Car {
 
 	public static void main(String[] args) {
 		
-		Car.brake();
+		Car car1 = new Car();
+		System.out.println(car1.toString());
+		System.out.println(car1.accelerate());
 		System.out.println(Car.brake());
 		
-		Car audi = new Car();
-		System.out.println(audi.accelerate());
-		audi.accelerate();
+		Car car2 = new Car("Q5");
+		System.out.println(car2.toString());
+		System.out.println(car2.accelerate());
+		System.out.println(Car.brake());
 		
-		System.out.println(audi.toString());
 	}
 }
